cb9912071323af63f7e073e285f7e48c72ef29fa
98456uno <taniyapaul2271@gmail.com>
2025-07-02 09:42:15 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,9 +1,9 @@
 # requirements.py
 with open("requirements.txt", "w") as f:
-    f.write("streamlit==1.46.1\n")
-    f.write("pandas==2.2.2\n")
-    f.write("matplotlib==3.10.3\n")
-    f.write("seaborn==0.13.2\n")
+    f.write("streamlit\n")
+    f.write("pandas\n")
+    f.write("matplotlib\n")
+    f.write("seaborn\n")
     f.write("pillow\n")  
 print("✅ requirements.txt created successfully!")
 
